d2f27149ad1656f071f4da4ab17cf3f27745c188
AminKhajavi <141088687+aminKhajavii@users.noreply.github.com>
2024-05-08 04:28:53 +0330

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -39,11 +39,11 @@ const Footer = () => {
                     <ul>
                         <li>
                             <span><i className = "fas fa-phone"></i></span>
-                            <span className = "fs-15">+678 004 5754</span>
+                            <span className = "fs-15">09015680976</span>
                         </li>
                         <li>
                             <span><i className = "fas fa-envelope"></i></span>
-                            <span className = "fs-15">info@shophub.com</span>
+                            <span className = "fs-15">info@amin82khajavi.com</span>
                         </li>
                     </ul>
                 </div>
